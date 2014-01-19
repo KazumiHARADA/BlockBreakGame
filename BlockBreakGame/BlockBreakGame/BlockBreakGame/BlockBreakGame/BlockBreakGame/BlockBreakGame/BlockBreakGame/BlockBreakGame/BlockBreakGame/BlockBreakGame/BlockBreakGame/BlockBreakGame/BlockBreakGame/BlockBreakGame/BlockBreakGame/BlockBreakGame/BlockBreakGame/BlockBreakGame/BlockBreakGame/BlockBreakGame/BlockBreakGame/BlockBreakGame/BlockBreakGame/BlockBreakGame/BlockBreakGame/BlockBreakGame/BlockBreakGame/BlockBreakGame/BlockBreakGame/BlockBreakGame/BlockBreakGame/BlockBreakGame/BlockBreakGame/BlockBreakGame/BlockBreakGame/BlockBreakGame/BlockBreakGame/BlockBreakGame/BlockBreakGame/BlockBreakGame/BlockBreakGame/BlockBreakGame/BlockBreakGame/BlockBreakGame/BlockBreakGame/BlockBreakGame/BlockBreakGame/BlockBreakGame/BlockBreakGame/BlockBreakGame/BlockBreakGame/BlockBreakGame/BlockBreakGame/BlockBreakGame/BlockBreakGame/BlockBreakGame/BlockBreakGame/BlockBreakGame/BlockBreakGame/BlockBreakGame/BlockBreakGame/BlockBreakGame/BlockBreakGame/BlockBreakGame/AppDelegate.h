ef77161e95d10ed403e74abda602edb57e730b4f
Kazumi HARADA <kazumi.h.apple48@gmail.com>
2014-01-19 23:22:55 +0900

    folder structure

---
+++ BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/BlockBreakGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlockBreakGame
+//
+//  Created by 原田一美 on 2014/01/19.
+//  Copyright (c) 2014年 kazumi harada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
