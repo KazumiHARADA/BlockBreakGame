c7bd7ba4cafa16e88b81bac593f8fd663bea49fa
Kazumi HARADA <kazumi.h.apple48@gmail.com>
2014-01-19 23:16:20 +0900

    create project

---
+++ BlockBreakGame/BlockBreakGame/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  BlockBreakGame
+//
+
+//  Copyright (c) 2014年 kazumi harada. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ BlockBreakGame/BlockBreakGameTests/BlockBreakGameTests.m
@@ -0,0 +1,34 @@
+//
+//  BlockBreakGameTests.m
+//  BlockBreakGameTests
+//
+//  Created by 原田一美 on 2014/01/19.
+//  Copyright (c) 2014年 kazumi harada. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BlockBreakGameTests : XCTestCase
+
+@end
+
+@implementation BlockBreakGameTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
